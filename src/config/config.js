7f344d403c8c522921be02d2cf4c71cc543b0343
Ihor Yatsyshyn <74863323+Yatsa-dev@users.noreply.github.com>
2023-05-25 22:24:00 +0300

    minor fix

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,3 @@
-// export const BASE_URL = 'https://yatsa-shop-back-fk6pcl23iq-uc.a.run.app/'
+export const BASE_URL = 'https://yatsa-shop-back-fk6pcl23iq-uc.a.run.app/';
 export const GOOGLE_CLIENT_ID =
   '161439927957-jo7oo4qpc1cg2h3mip1asr0bbng436rm.apps.googleusercontent.com';
-
-export const BASE_URL = 'http://localhost:3000/';
