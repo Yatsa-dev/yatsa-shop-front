d055beddac983c7d83f1d4e78819dcadbc3dbee8
Ihor Yatsyshyn <74863323+Yatsa-dev@users.noreply.github.com>
2023-05-27 18:15:27 +0300

    change base url

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,3 @@
-export const BASE_URL = 'https://yatsa-shop-back-fk6pcl23iq-uc.a.run.app/';
+export const BASE_URL = 'https://yatsa-shop-back-fk6pcl23iq-lm.a.run.app/';
 export const GOOGLE_CLIENT_ID =
   '161439927957-jo7oo4qpc1cg2h3mip1asr0bbng436rm.apps.googleusercontent.com';
